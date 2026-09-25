72317568bbe2b3ffc08b9acbb9d61a5be65085be
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 09:18:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
@@ -10,11 +9,16 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改后台默认登录 IP 为 192.168.1.1（如需改成 192.168.6.1，可解开下面注释）
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 注入 360T7 (MT7981 / filogic) 内核 BTF 与 eBPF 配置
+echo "CONFIG_DEBUG_INFO=y" >> target/linux/mediatek/filogic/config-default
+echo "CONFIG_DEBUG_INFO_BTF=y" >> target/linux/mediatek/filogic/config-default
+echo "CONFIG_BPF=y" >> target/linux/mediatek/filogic/config-default
+echo "CONFIG_BPF_SYSCALL=y" >> target/linux/mediatek/filogic/config-default
+echo "CONFIG_NET_CLS_ACT=y" >> target/linux/mediatek/filogic/config-default
+echo "CONFIG_NET_SCH_INGRESS=y" >> target/linux/mediatek/filogic/config-default
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 添加 daed 源码仓库
+git clone https://github.com/sbwml/luci-app-daed package/daed
