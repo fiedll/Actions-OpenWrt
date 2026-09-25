fe785ea7682a9c289670ad757f0507711af57c9b
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 18:38:17 +0800

    Update default login IP and password in script

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,7 +3,10 @@
 # 1. 修改后台默认登录 IP 为 192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 2. 注入 360T7 (filogic) 内核 BTF 与 eBPF 配置（让官方内核完美支持 daed）
+# 2. 强制设置默认密码为 password
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CY6rgitp3R5hkppTra52TW3:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# 3. 注入 MT7981 平台内核 BTF 与 eBPF 配置
 echo "CONFIG_DEBUG_INFO=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_DEBUG_INFO_BTF=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_BPF=y" >> target/linux/mediatek/filogic/config-default
@@ -11,5 +14,5 @@ echo "CONFIG_BPF_SYSCALL=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_NET_CLS_ACT=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_NET_SCH_INGRESS=y" >> target/linux/mediatek/filogic/config-default
 
-# 3. 拉取 daed 源码
+# 4. 拉取 daed 源码
 git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/daed
