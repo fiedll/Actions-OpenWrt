d26799b84fe982bd1640c23ac4c1b57569791fbd
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 11:31:49 +0800

    Update diy-part2.sh for IP and kernel version changes

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # 1. 修改后台默认登录 IP 为 192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
@@ -11,7 +6,7 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 2. 强制指定 mediatek/filogic 平台内核版本为 6.12
 sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.12/g' target/linux/mediatek/Makefile
 
-# 3. 注入 MT7981 (filogic) 内核 BTF 与 eBPF 配置（解决 daed 的 no BTF found）
+# 3. 注入 360T7 (filogic) 内核 BTF 与 eBPF 配置
 echo "CONFIG_DEBUG_INFO=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_DEBUG_INFO_BTF=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_BPF=y" >> target/linux/mediatek/filogic/config-default
@@ -19,13 +14,5 @@ echo "CONFIG_BPF_SYSCALL=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_NET_CLS_ACT=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_NET_SCH_INGRESS=y" >> target/linux/mediatek/filogic/config-default
 
-# 4. 添加第三方软件包源
-# daed
-git clone https://github.com/sbwml/luci-app-daed package/daed
-
-# MosDNS
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-
-# PassWall 源码及其核心依赖
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+# 4. 仅拉取必须的 daed 源码（PassWall / MosDNS 官方自带，无需在此克隆）
+git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/daed
