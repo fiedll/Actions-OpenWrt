22d6dcc428ea035e64178cb40ffb2428df3228cc
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 23:20:00 +0800

    Refactor dropbear setup and eBPF configuration
    Refactor dropbear configuration setup and ensure eBPF options are added only if not present. Clean up unnecessary commands and remove the 99-custom-settings file after execution.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
@@ -8,26 +7,26 @@ mkdir -p package/base-files/files/etc/uci-defaults
 cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<'EOF'
 #!/bin/sh
 echo "root:password" | chpasswd
-uci set dropbear.@dropbear[0].RootPasswordAuth='1'
-uci set dropbear.@dropbear[0].PasswordAuth='1'
-uci commit dropbear
-
+if uci show dropbear.@dropbear[0] >/dev/null 2>&1; then
+    uci set dropbear.@dropbear[0].RootPasswordAuth='1'
+    uci set dropbear.@dropbear[0].PasswordAuth='1'
+    uci commit dropbear
+fi
 [ -f /etc/init.d/dropbear ] && {
     /etc/init.d/dropbear enable
     /etc/init.d/dropbear restart
 }
-
 [ -f /etc/init.d/uhttpd ] && {
     /etc/init.d/uhttpd enable
     /etc/init.d/uhttpd restart
 }
-
+rm -f /etc/uci-defaults/99-custom-settings
 exit 0
 EOF
 chmod +x package/base-files/files/etc/uci-defaults/99-custom-settings
 
 # eBPF
-cat >> target/linux/mediatek/filogic/config-default <<EOF
+grep -q "CONFIG_BPF=y" target/linux/mediatek/filogic/config-default || cat >> target/linux/mediatek/filogic/config-default <<EOF
 CONFIG_DEBUG_INFO=y
 CONFIG_DEBUG_INFO_BTF=y
 CONFIG_DEBUG_INFO_DWARF4=y
