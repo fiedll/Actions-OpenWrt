67e1db0648154368c8dc75809314d2dbb3e234b8
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 23:31:27 +0800

    Refactor diy-part2.sh for better readability and structure

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' \
+package/base-files/files/bin/config_generate
 
 # 首次启动脚本
 mkdir -p package/base-files/files/etc/uci-defaults
@@ -9,41 +10,60 @@ mkdir -p package/base-files/files/etc/uci-defaults
 cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<'EOF'
 #!/bin/sh
 
+# 设置默认密码
 echo "root:password" | chpasswd
 
+# 开启SSH密码登录
 if uci show dropbear.@dropbear[0] >/dev/null 2>&1; then
     uci set dropbear.@dropbear[0].RootPasswordAuth='1'
     uci set dropbear.@dropbear[0].PasswordAuth='1'
     uci commit dropbear
 fi
 
+# 启动SSH
 [ -f /etc/init.d/dropbear ] && {
     /etc/init.d/dropbear enable
     /etc/init.d/dropbear restart
 }
 
+# 启动Web
 [ -f /etc/init.d/uhttpd ] && {
+    uci set uhttpd.main.listen_http='0.0.0.0:80'
+    uci set uhttpd.main.redirect_https='0'
+    uci commit uhttpd
+
     /etc/init.d/uhttpd enable
     /etc/init.d/uhttpd restart
 }
 
+# ttyd支持
+if uci show ttyd.@ttyd[0] >/dev/null 2>&1; then
+    uci set ttyd.@ttyd[0].command='/bin/login'
+    uci commit ttyd
+fi
+
 rm -f /etc/uci-defaults/99-custom-settings
+
 exit 0
 EOF
 
-chmod +x package/base-files/files/etc/uci-defaults/99-custom-settings
+chmod +x \
+package/base-files/files/etc/uci-defaults/99-custom-settings
 
-# eBPF
-grep -q "CONFIG_BPF=y" target/linux/mediatek/filogic/config-default || cat >> target/linux/mediatek/filogic/config-default <<EOF
-CONFIG_DEBUG_INFO=y
-CONFIG_DEBUG_INFO_BTF=y
-CONFIG_DEBUG_INFO_DWARF4=y
+# eBPF支持
+grep -q "^CONFIG_BPF=y" \
+target/linux/mediatek/filogic/config-default || cat >> \
+target/linux/mediatek/filogic/config-default <<'EOF'
+
 CONFIG_BPF=y
 CONFIG_BPF_SYSCALL=y
 CONFIG_NET_CLS_ACT=y
 CONFIG_NET_SCH_INGRESS=y
+
 EOF
 
-# daed
+# Daed
 rm -rf package/daed
-git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/daed
+git clone --depth=1 \
+https://github.com/QiuSimons/luci-app-daed \
+package/daed
