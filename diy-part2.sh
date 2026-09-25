af388fcd51b427d1531d1fbdc855d699301080ed
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 09:36:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,16 +4,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
 # 1. 修改后台默认登录 IP 为 192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 2. 注入 360T7 (MT7981 / filogic) 内核 BTF 与 eBPF 配置
+# 2. 强制指定 mediatek/filogic 平台内核版本为 6.12
+sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.12/g' target/linux/mediatek/Makefile
+
+# 3. 注入 MT7981 (filogic) 内核 BTF 与 eBPF 配置（解决 no BTF found 核心）
 echo "CONFIG_DEBUG_INFO=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_DEBUG_INFO_BTF=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_BPF=y" >> target/linux/mediatek/filogic/config-default
@@ -21,5 +19,5 @@ echo "CONFIG_BPF_SYSCALL=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_NET_CLS_ACT=y" >> target/linux/mediatek/filogic/config-default
 echo "CONFIG_NET_SCH_INGRESS=y" >> target/linux/mediatek/filogic/config-default
 
-# 3. 添加 daed 源码仓库
+# 4. 添加 daed 源码仓库
 git clone https://github.com/sbwml/luci-app-daed package/daed
