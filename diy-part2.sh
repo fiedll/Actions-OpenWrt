12a98c084112f63bfd8cb230158948146e94b258
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-25 23:22:07 +0800

    Update IP and add initial setup for services
    Modify default IP, add initial setup script, enable services, and clone daed package.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 首次启动脚本
 mkdir -p package/base-files/files/etc/uci-defaults
+
 cat > package/base-files/files/etc/uci-defaults/99-custom-settings <<'EOF'
 #!/bin/sh
+
 echo "root:password" | chpasswd
+
 if uci show dropbear.@dropbear[0] >/dev/null 2>&1; then
     uci set dropbear.@dropbear[0].RootPasswordAuth='1'
     uci set dropbear.@dropbear[0].PasswordAuth='1'
     uci commit dropbear
 fi
+
 [ -f /etc/init.d/dropbear ] && {
     /etc/init.d/dropbear enable
     /etc/init.d/dropbear restart
 }
+
 [ -f /etc/init.d/uhttpd ] && {
     /etc/init.d/uhttpd enable
     /etc/init.d/uhttpd restart
 }
+
 rm -f /etc/uci-defaults/99-custom-settings
 exit 0
 EOF
+
 chmod +x package/base-files/files/etc/uci-defaults/99-custom-settings
 
 # eBPF
