824be1aad9c320323c9feb9b1bd522a829bcfcab
fiedll <54751587+fiedll@users.noreply.github.com>
2026-09-26 08:27:53 +0800

    Clean up diy-part1.sh by removing unnecessary lines
    Removed comments and git clone command from the script.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# 拉取 daed 源码仓库至 package 目录
-#!/bin/bash
-
-# 拉取最新的 daede 源码仓库至 package 目录
-git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/daed
+# 这里保持干净，不要写 git clone
